5b0339fe2e73a2c30241815e95e1abfaa100fac1
curevack <43734358+curevack@users.noreply.github.com>
2018-11-11 19:48:19 -0800

    Create function-basics.js

---
+++ javascripts-tests/function-basics.js
@@ -0,0 +1,17 @@
+window.onload = {
+  function simpleFunction {
+     console.log('functions should consist of statements designed to perform a single task.');
+     simpleFunction.textcontent = 'functions should consist of statements designed to perform a single task.';
+     }
+  function functionParameters (input) {
+     console.log(input);
+     }
+  function functionReturn {
+     return 'Many functions return values.';
+     }
+  simpleFunction();
+  functionParameters('Many functions take parameters.');
+  functionReturn();
+}
+      
+
