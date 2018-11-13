693c5557c7d54bc248250444bad44b545e223102
curevack <43734358+curevack@users.noreply.github.com>
2018-11-12 18:24:06 -0800

    Update function-basics.js

--- javascripts-tests/function-basics.js
+++ javascripts-tests/function-basics.js
@@ -3,27 +3,23 @@ window.onload = {
   const functionParameters = document.getElementById('function-parameters');
   const functionReturn = document.getElementById('function-return');
   const functionCalls = document.getElementById('function-calls');
-  
+    simpleFunction();
+    functionParameters("Many functions take parameters.")
+    functionReturn();
+    functionCalls.onclick = simpleFunction, functionParameters, functionReturn;
+  }
+
   function simpleFunction {
      console.log('functions should consist of statements designed to perform a single task.');
      simpleFunction.textContent = 'functions should consist of statements designed to perform a single task.';
-      functionCalls.onclick = simpleFunction;
      }
-  functionCalls.onclick = simpleFunction;
   function functionParameters (input) {
      console.log(input);
      functionParameters.textContent = input;
      }
-  functionCalls.onclick = functionParameters;
   function functionReturn {
      functionReturn.textContent = 'Many functions return values.';
      return 'Many functions return values.';
      }
-  functionCalls.onclick = functionReturn;
-
-  simpleFunction();
-  functionParameters('Many functions take parameters.');
-  functionReturn();
-}
       
 
