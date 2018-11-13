7679a5ba9026b90ed0df8c86967d8f4c90712536
curevack <43734358+curevack@users.noreply.github.com>
2018-11-12 19:51:46 -0800

    Update function-basics.js

--- javascripts-tests/function-basics.js
+++ javascripts-tests/function-basics.js
@@ -1,26 +1,23 @@
 window.onload = () => {
-  const simpleFunction = document.getElementById('simple-function');
-  const functionParameters = document.getElementById('function-parameters');
-  const functionReturn = document.getElementById('function-return');
-  const functionCalls = document.getElementById('function-calls');
-    functionCalls.onclick = () => {
-      simpleFunction();
-      functionParameters(); 
-      functionReturn();
+    const simpleFunction = document.getElementById('simple-function');
+    const functionParameters = document.getElementById('function-parameters');
+    const functionReturn = document.getElementById('function-return');
+    const functionCalls = document.getElementById('function-calls');
+      functionCalls.onclick = () => {
+        simpleFunction.textContent = simpleFunction();
+        functionParameters.textContent = functionParameters('Many functions take parameters.');
+        functionReturn.textContent= functionReturn();
+      }
+      function simpleFunction () {
+        console.log('functions should consist of statements designed to perform a single task.');
+        simpleFunction.textContent = 'functions should consist of statements designed to perform a single task.';
+        }
+     function functionParameters (input) {
+        console.log(input);
+        functionParameters.textContent = input;
+        }
+     function functionReturn () {
+        functionReturn.textContent = 'Many functions return values.';
+        return 'Many functions return values.';
+        }
     }
-  }
-
-  function simpleFunction () {
-     console.log('functions should consist of statements designed to perform a single task.');
-     simpleFunction.textContent = 'functions should consist of statements designed to perform a single task.';
-     }
-  function functionParameters (input) {
-     console.log(input);
-     functionParameters.textContent = input;
-     }
-  function functionReturn () {
-     functionReturn.textContent = 'Many functions return values.';
-     return 'Many functions return values.';
-     }
-      
-
