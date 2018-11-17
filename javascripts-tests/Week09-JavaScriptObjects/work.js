90f8dcf99477bf785ed96e3505f1812293118396
curevack <43734358+curevack@users.noreply.github.com>
2018-11-16 16:04:43 -0800

    Update work.js

--- javascripts-tests/Week09-JavaScriptObjects/work.js
+++ javascripts-tests/Week09-JavaScriptObjects/work.js
@@ -1,1 +1,43 @@
+  function divider(title) {
+  console.log("====================================");
+  console.log(title);
+  console.log("====================================");
+  }
+  
+  console.log(divider('Person'));
 
+  var person = {
+    firstName: 'Kerim',
+    lastName: 'Curevac',
+    fullName: function () {
+        return this.firstName+ " "+ this.lastName;
+    }
+  };
+  console.log(person.firstName);
+  console.log(person.lastName);
+  console.log(person.fullName());
+  
+  console.log(divider('Calculator'));
+
+  var calculator = {
+    operand01: -1,
+    operand02: -1,
+    add: function() {
+      return "Add: " + (calculator.operand01 + calculator.operand02);
+    },
+    subtract: function () {
+      return "Subtract: " + (calculator.operand01 - calculator.operand02);
+    }
+  };
+  calculator.operand01 = person.firstName.length;
+  calculator.operand02 = person.lastName.length;
+  calculator.multiply = calculator.operand01 * calculator.operand02;
+  console.log('Operand01 = '+ calculator.operand01);
+  console.log('Operand02 = '+ calculator.operand02);
+  console.log(calculator.add());
+  console.log(calculator.subtract());
+  console.log('Multiply = '+ calculator.multiply);
+
+  
+
+  
