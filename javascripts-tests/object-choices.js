5ae31406b240d30d5a3429aa0818562d265d6a08
curevack <43734358+curevack@users.noreply.github.com>
2018-11-18 20:18:12 -0800

    Update object-choices.js

--- javascripts-tests/object-choices.js
+++ javascripts-tests/object-choices.js
@@ -1,3 +1,33 @@
+ window.onload = () => {
+  const simpleObjectAction = 
+        document.getElementById('simple-object-action');
+  
+  const functionObjectAction = 
+        document.getElementById('function-object-action');
+  
+  const customClassAction = 
+        document.getElementById('custom-class-action');
+
+    simpleObjectAction.onclick = () => {
+        const simpleObjectDisplay = document.getElementById('simple-object-display');
+
+        simpleObjectDisplay.textContent = simpleObject.sayName(); + " " + simpleObject.dynamicMethod;
+    }; 
+
+    functionObjectAction.onclick = () => {
+        const functionObjectDisplay = document.getElementById('function-object-display');
+
+        functionObjectDisplay.textContent = "Private Function";
+    }; 
+ 
+    customClassAction.onclick = () => {
+        const customClassDisplay = document.getElementById('custom-class-display');
+
+        customClassDisplay.textContent = "Custom Class";
+    };
+  
+  };
+
     class Custom_Class {
         giveString() {
             return 'Custom Class';
