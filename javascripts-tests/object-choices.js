18f6b06f1d9ff6340f66e57cdf9bd28a4fcfc278
curevack <43734358+curevack@users.noreply.github.com>
2018-11-18 19:53:19 -0800

    Update object-choices.js

--- javascripts-tests/object-choices.js
+++ javascripts-tests/object-choices.js
@@ -1,1 +1,33 @@
-s
+    class Custom_Class {
+        giveString() {
+            return 'Custom Class';
+        }
+    } 
+    
+    var simpleObject = {
+    sayName: function () {
+       return "Simple Object.";
+        }
+    }
+
+ function FunctionObject() {
+     this.privateFunction = function () {
+         return "Private Function";
+     }
+    }
+    
+ FunctionObject.prototype.sayName = function () {
+        return this.privateFunction();
+        }
+
+    var functionObject = new FunctionObject();
+    functionObject.sayName();
+    
+    simpleObject.sayName();
+    simpleObject.dynamicMethod;
+
+    console.log(simpleObject.sayName());
+    simpleObject.dynamicMethod = console.log("Dynamic Method"); 
+    console.log(functionObject.privateFunction());
+    console.log(new Custom_Class().giveString());
+    
